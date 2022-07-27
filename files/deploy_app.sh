76ebd38acad08dbde43942d6f1e24e09589a65bd
patriciarojasp <patricia.rojasperez@hashicorp.com>
2022-07-27 15:42:44 -0500

    random puppies every time you click

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -9,9 +9,9 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}?r"></img></center>
   <center><h2>Woolf World!</h2></center>
-  Welcome to ${PREFIX}'s app. Puppies are BETTER than cats! & Yes, all dogs are puppies no matter their age.
+  <center>Welcome to ${PREFIX}'s app. Puppies are BETTER than cats! & Yes, all dogs are puppies no matter their age.</center>
   <!-- END -->
 
   </div>
